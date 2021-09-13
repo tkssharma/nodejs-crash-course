18ccf6207d71bbfa58c4d3c7c61d32b40f501883
tkssharma <tarun.softengg@gmail.com>
2021-09-13 11:38:58 +0530

    updates

--- 04.2-express-app/app.js
+++ 04.2-express-app/app.js
@@ -48,6 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/cat', cat);
 // users --> users  route handler
 //  / index route handler 
 // catch 404 and forward to error handler
